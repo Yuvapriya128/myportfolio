264a4862d8e21d986c248eb365fc0db9b33182f9
Yuvapriya128 <yuvapriya128@gmail.com>
2025-04-27 11:21:02 +0530

    Remove basename from HashRouter to fix routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // Use HashRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>  {/* Ensure the basename is set here */}
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
